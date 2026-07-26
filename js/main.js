1243f3b1e01ca5dd8b065428aef78ff2199c57df
patapon98 <alexanderblanc3@gmail.com>
2026-07-26 19:51:14 +0900

    Carga el nuevo mensaje y SEO de la portada

--- js/main.js
+++ js/main.js
@@ -63,9 +63,12 @@
   load('site-nav.js');
   load('home-news-copy.js');
   load('home-analysis-links.js');
-  load('main-core.js',()=>load('calendar-images.js',()=>load('calendar-labels.js',()=>{
-    load('calendar-trailers.js');
-    load('calendar-gcal.js');
-    load('calendar-mosaic.js',()=>load('calendar-view.js'));
-  })));
+  load('main-core.js',()=>{
+    load('home-brand-copy.js');
+    load('calendar-images.js',()=>load('calendar-labels.js',()=>{
+      load('calendar-trailers.js');
+      load('calendar-gcal.js');
+      load('calendar-mosaic.js',()=>load('calendar-view.js'));
+    }));
+  });
 })();

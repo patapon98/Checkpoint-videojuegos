2ae9b4e4b1066772ea7372fd8bcc03323562b52e
patapon98 <alexanderblanc3@gmail.com>
2026-07-29 11:08:42 +0900

    Renueva la caché del calendario

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@
   };
 
   load('home-analysis-links.js?v=20260728-1');
-  load('calendar-today.js?v=20260729-1',()=>{
+  load('calendar-today.js?v=20260729-2',()=>{
     load('calendar-countdown-prepare.js?v=20260729-1',()=>{
       load('main-core.js?v=20260729-6',()=>{
         load('calendar-countdown.js?v=20260729-1');

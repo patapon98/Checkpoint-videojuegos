c163fbac0a764f19dbd85a7e7c2aa032158535ff
patapon98 <alexanderblanc3@gmail.com>
2026-09-10 10:12:43 +0900

    Muestra el ticker solo tras hidratarlo

--- js/news.js
+++ js/news.js
@@ -1,4 +1,14 @@
 (function () {
+  const ticker = document.getElementById("ticker");
+  let tickerObserver = null;
+  if (ticker) {
+    tickerObserver = new MutationObserver(() => {
+      ticker.classList.add("is-hydrated");
+      tickerObserver?.disconnect();
+    });
+    tickerObserver.observe(ticker, { childList: true });
+  }
+
   const script = document.createElement("script");
   const loaderUrl = new URL(document.currentScript?.src || location.href, location.href);
   const coreUrl = new URL("/js/news-core.js", location.href);
@@ -6,6 +16,8 @@
   if (version) coreUrl.searchParams.set("v", version);
   script.src = coreUrl;
   script.onerror = function () {
+    tickerObserver?.disconnect();
+    ticker?.classList.add("is-hydrated");
     console.error("No se pudo cargar el motor de Noticias.");
   };
   document.head.appendChild(script);
